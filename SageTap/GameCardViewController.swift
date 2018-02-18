f7f897d4a0cfb450f99cf65b97824cc729aa97d6
Matt Scott <mjscott4242@gmail.com>
2018-02-17 23:01:41 -0600

    Have data loading on initial card load

--- SageTap/GameCardViewController.swift
+++ SageTap/GameCardViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 import MediaPlayer
-
+import Firebase
 
 class GameCardViewController: UIViewController {
 
@@ -14,56 +14,61 @@ class GameCardViewController: UIViewController {
     //MPMoviePlayerViewController * _player;
     
 
+    //Outlets
+    @IBOutlet weak var timerOutlet: UILabel!
+    @IBOutlet weak var questionOutlet: UILabel!
+    @IBOutlet weak var option1Outlet: UIButton!
+    @IBOutlet weak var option2Outlet: UIButton!
+    @IBOutlet weak var option3Outlet: UIButton!
+    @IBOutlet weak var option4Outlet: UIButton!
     
-    @IBOutlet weak var timer: UILabel!
-    @IBOutlet weak var question: UILabel!
+    
+    //Class vars
     var options = [UIButton]()
+    var activeCardIndex = ""
+    var activeCard:GameCard = GameCard()
+    var game_on = false
     
-    @IBOutlet weak var option1: UIButton!
-    @IBOutlet weak var option2: UIButton!
-    @IBOutlet weak var option3: UIButton!
-    @IBOutlet weak var option4: UIButton!
+    //firebase ref
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
         //Initialize variables
+        self.options.append(option1Outlet)
+        self.options.append(option2Outlet)
+        self.options.append(option3Outlet)
+        self.options.append(option4Outlet)
+        self.ref = Database.database().reference()
         
-        self.options.append(option1)
-        self.options.append(option2)
-        self.options.append(option3)
-        self.options.append(option4)
+        self.getQuestion()
         
-        self.getQuestion();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    /*
-    @IBAction func playBtnTapped(_ sender: Any) {
-        //play button code
-        NSURL = [NSURL (fileURLWithPath: ""<#T##String#>"")]
-        _player =
-    }*/
     
     func getQuestion()
     {
-        print("in function")
+    
+        self.ref.child("activeCard").observe(DataEventType.value, with: { (snapshot) in
+            
+            //print snapshot.toString()
+            self.activeCardIndex = snapshot.value as! String
+            
+            self.ref.child("cards").child(self.activeCardIndex).observeSingleEvent(of: .value, with: { (card_snap) in
+                let dataChange = card_snap.value as? [String:AnyObject]
+                self.activeCard = GameCard()
+                self.activeCard.initWithDict(aDict: dataChange!)
+            })
+            
+        })
     }
+
     
 }
